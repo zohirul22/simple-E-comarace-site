cffe4654616fd2709975f49b3f0b922433165833
zohirulislam1 <codershahin84@gmail.com>
2022-03-25 23:31:05 +0600

    2nd time commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Product from './components/Products/Product';
 
 function App() {
   return (
     <div className="App">
-    
+   <Product></Product>
     </div>
   );
 }
---
+++ src/components/Products/Product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Product = () => {
+    return (
+        <div>
+            <h1>Shopping Shoes</h1>
+            
+        </div>
+    );
+};
+
+export default Product;
